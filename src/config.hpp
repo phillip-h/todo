2e3b3dac2a172332e095203d350dd31754c6c2a6
Phillip Heikoop <phillip-h@users.noreply.github.com>
2016-01-27 16:00:50 -0700

    changed version number to 0.8_dev

--- src/config.hpp
+++ src/config.hpp
@@ -5,7 +5,7 @@
 #include <string>
 
 // version number
-const std::string vn = "0.8";
+const std::string vn = "0.8_dev";
 
 // the default name of a todo list, used if one isn't provided at runtime
 const std::string DEFAULT_NAME = "todo";
